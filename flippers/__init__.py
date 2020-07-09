3f277e48278b80cef8aaa798096102e6b43f53a2
Marcin Gasiewicz <margasiewicz@gmail.com>
2020-07-09 15:33:09 +0200

    add login.html register.html and bcrypt

--- flippers/__init__.py
+++ flippers/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 from secret_key import SECRET_KEY
 
 
@@ -8,7 +9,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
