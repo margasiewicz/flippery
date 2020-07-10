e03bca277be93937a2e3a5ef1d2b20b29b7da887
Marcin Gasiewicz <margasiewicz@gmail.com>
2020-07-10 12:19:13 +0200

    add API addcredit POST method

--- flippers/routes.py
+++ flippers/routes.py
@@ -1,10 +1,10 @@
-from flask import render_template
+from flask import render_template, url_for, flash, redirect
 from flippers.forms import RegistrationForm, LoginForm
 from flask_login import login_user, current_user, logout_user, login_required
 
 from flippers.models import User, Msgs
 from flippers import app, db, bcrypt
-
+import json
 
 
 
@@ -17,14 +17,14 @@ def index():
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        flash('Your account has been created! You are now able to log in', 'success')
+        flash('Account created!', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
@@ -32,19 +32,42 @@ def register():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('home'))
+            return redirect(url_for('index'))
         else:
-            flash('Login Unsuccessful. Please check email and password', 'danger')
+            flash('Wrong email or password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 @app.route("/logout")
 def logout():
     logout_user()
-    return redirect(url_for('home'))
+    return redirect(url_for('index'))
+
+
+'''
+API FROM HERE
+'''
+
+@app.route('/addcredit', methods=['POST'])
+@login_required
+def addcredit():
+    '''
+    Remember to set Timer: Auto Off in Shelly App
+    auth_key: Shelly1 auth_key
+    id : Shelly1 id
+    '''
+
+    json_data = {
+    "auth_key":"<AUTH_KEY here>",
+    "id":"<ID here>",
+    "channel":"0",
+    "turn":"on"
+    }
+
+    r1 = requests.post('https://shelly-12-eu.shelly.cloud/device/relay/control',data=json_data)
+    return r1.json()
