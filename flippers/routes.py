f68b27c5d0e740e1b3478d85339770c57dbd25e9
Marcin Gasiewicz <margasiewicz@gmail.com>
2020-07-13 14:36:47 +0200

    add comments and commented out register route

--- flippers/routes.py
+++ flippers/routes.py
@@ -20,20 +20,24 @@ def index():
     data= [int(worth_2zl), int(worth_5zl)]
     return render_template("index.html", data=data)
 
-    
-@app.route("/register", methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(user)
-        db.session.commit()
-        flash('Account created!', 'success')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+'''
+uncomment register route if you wish to have it
+available. It has no authentication.
+'''
+
+# @app.route("/register", methods=['GET', 'POST'])
+# def register():
+#     if current_user.is_authenticated:
+#         return redirect(url_for('index'))
+#     form = RegistrationForm()
+#     if form.validate_on_submit():
+#         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+#         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+#         db.session.add(user)
+#         db.session.commit()
+#         flash('Account created!', 'success')
+#         return redirect(url_for('login'))
+#     return render_template('register.html', title='Register', form=form)
 
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -94,8 +98,8 @@ def addcredit():
     return r1.json()
 '''
 Remember to set Timer: Auto Off in Shelly App
-auth_key: Shelly1 auth_key
-id : Shelly1 id
+auth_key: your Shelly1 auth_key
+id : your Shelly1 id
 '''
 
 @app.route('/delete2', methods=['GET'])
