5cd2ccc90d821db0b73daaa6fff873f3a144bb49
Marcin Gasiewicz <margasiewicz@gmail.com>
2020-08-13 22:03:26 +0200

    add loginTestData and html reports

--- tests/testCases/conftest.py
+++ tests/testCases/conftest.py
@@ -5,3 +5,10 @@ import pytest
 def setup():
     driver = webdriver.Chrome()
     return driver
+
+# PyTest HTML Reports 
+
+def pytest_configure(config):
+    config._metadata['Project Name'] = 'Flippers remote control'
+    config._metadata['Module Name'] = 'Flippers'
+    config._metadata['Tester'] = 'margasiewicz'
--- tests/testCases/test_login.py
+++ tests/testCases/test_login.py
@@ -6,7 +6,9 @@ class Test_001_Login:
     baseURL = "http://127.0.0.1:5000/login"
     username = 'admin@gmail.com'
     password = 'admin123'
-
+    falseUsername = 'admin111@gmail.com'
+    falseUsername = 'admin111@gmail.com'
+    
     def test_loginPageTitle(self, setup):
         self.driver = setup
         self.driver.get(self.baseURL)
@@ -30,4 +32,17 @@ class Test_001_Login:
             assert True
         else:
             assert False
-
+            
+    def test_loginFailure(self, setup):
+        self.driver = setup
+        self.driver.get(self.baseURL)
+        self.lp = Login(self.driver)
+        self.lp.setUserName(self.username)
+        self.lp.setUserPassword(self.password)
+        self.lp.clickLogin()
+        act_title=self.driver.title
+        self.driver.close()
+        if act_title=='Flippers':
+            assert True
+        else:
+            assert False
