6bcf19d6eaad41a19a24e1c0c72e24d39925fdbc
Marcin Gasiewicz <margasiewicz@gmail.com>
2020-08-13 21:35:57 +0200

    add Test_001_Login class to test_login.py

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -5,28 +5,28 @@ from pageObjects.loginPage import Login
 class Test_001_Login:
     baseURL = "http://127.0.0.1:5000/login"
     username = 'admin@gmail.com'
-    password = 'admin'
+    password = 'admin123'
 
     def test_loginPageTitle(self, setup):
         self.driver = setup
         self.driver.get(self.baseURL)
         act_title=self.driver.title
         self.driver.close()
-        if act_tite=='Flippers - Login':
+        if act_title=='Flippers - Login':
             assert True
         else:
             assert False
 
-    def test_login(self):
+    def test_login(self, setup):
         self.driver = setup
         self.driver.get(self.baseURL)
-        self.lp = Login()
+        self.lp = Login(self.driver)
         self.lp.setUserName(self.username)
         self.lp.setUserPassword(self.password)
         self.lp.clickLogin()
         act_title=self.driver.title
         self.driver.close()
-        if act_tite=='Flippers':
+        if act_title=='Flippers':
             assert True
         else:
             assert False
