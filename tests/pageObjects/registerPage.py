c11274f40339d6043953c3e2d949bf2153ba5436
Marcin Gasiewicz <margasiewicz@gmail.com>
2020-08-17 22:46:37 +0200

    add registerPage.py with Register class

---
+++ tests/pageObjects/registerPage.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+
+
+class Register:
+    textbox_username_id = 'username'
+    textbox_email_id = 'email'
+    textbox_password_id = 'password'
+    textbox_conf_password_id = 'confirm_password'
+    button_register_xpath = '//*[@id="submit"]'
+    
+    def __init__(self, driver):
+        self.driver = driver
+        
+    def setUserName(self, username):
+        self.driver.find_element_by_id(self.textbox_username_id).clear
+        self.driver.find_element_by_id(self.textbox_username_id).send_keys(username)
+        
+    def setUserEmail(self, email):
+        self.driver.find_element_by_id(self.textbox_email_id).clear
+        self.driver.find_element_by_id(self.textbox_email_id).send_keys(email)
+    
+    def setAndConfirmUserPassword(self, password, confPassword):
+        self.driver.find_element_by_id(self.textbox_password_id).clear
+        self.driver.find_element_by_id(self.textbox_password_id).send_keys(password)
+        self.driver.find_element_by_id(self.textbox_conf_password_id).clear
+        self.driver.find_element_by_id(self.textbox_conf_password_id).send_keys(confPassword)
+    
+    def clickSignUp(self):
+        self.driver.find_element_by_xpath(self.button_register_xpath).click()
